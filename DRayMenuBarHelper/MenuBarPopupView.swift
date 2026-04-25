8cffe8987d7590224d273f2323ce9e9b37f46b5f
squazaryu <tumowuh@internet.ru>
2026-04-25 14:34:01 +0300

    refactor(helper): isolate menu bar status icon view

--- DRayMenuBarHelper/MenuBarPopupView.swift
+++ DRayMenuBarHelper/MenuBarPopupView.swift
@@ -1,32 +1,6 @@
 import SwiftUI
 import AppKit
 
-struct MenuBarStatusIcon: View {
-    @ObservedObject var monitor: LiveSystemMetricsMonitor
-    @Environment(\.colorScheme) private var colorScheme
-
-    var body: some View {
-        HStack(spacing: 3) {
-            if let percent = monitor.snapshot.batteryLevelPercent {
-                Text("\(percent)%")
-                    .font(.system(size: 11, weight: .semibold, design: .rounded))
-                    .monospacedDigit()
-            } else {
-                Text("BAT")
-                    .font(.system(size: 10, weight: .semibold, design: .rounded))
-            }
-            if monitor.snapshot.batteryIsCharging == true {
-                Image(systemName: "bolt.fill")
-                    .font(.system(size: 9, weight: .bold))
-            }
-        }
-        .padding(.horizontal, 6)
-        .padding(.vertical, 2)
-        .contentShape(Rectangle())
-        .foregroundStyle(colorScheme == .dark ? .white : .black)
-    }
-}
-
 struct MenuBarPopupView: View {
     @ObservedObject var model: MenuBarPopupModel
     @ObservedObject var monitor: LiveSystemMetricsMonitor
